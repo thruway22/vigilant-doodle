01420cdc368207d0d72349435a33ea96335e2726
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-09-12 02:14:49 +0300

    u

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 
 with open('db.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = Authenticate(
+authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
