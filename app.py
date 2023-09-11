b9f31ec9434203566051cfa1fd362be2b150a663
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-09-12 02:13:19 +0300

    u

--- app.py
+++ app.py
@@ -5,4 +5,12 @@ from yaml.loader import SafeLoader
 with open('db.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
 st.title('test')
